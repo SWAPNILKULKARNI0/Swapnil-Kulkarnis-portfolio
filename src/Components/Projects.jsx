2f4082c5ef313b05ecfb841b9cb0af2cbed7b476
SWAPNIL KULKARNI <swapnil.kulkarni0000@gmail.com>
2024-02-15 00:53:41 +0530

    add links

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,7 +8,7 @@ import ongoing from '../assets/images/E-com.png'
 const Projects = () => {
   const weatherapp_project_live_link = "https://swapnilkulkarni0.github.io/Weather-WebApplication/";
   const weatherapp_project_sourcecode_link = "https://github.com/SWAPNILKULKARNI0/Weather-WebApplication.git"
-
+  const portfolio_project_sourcecode_link = "https://github.com/SWAPNILKULKARNI0/Swapnil-Kulkarnis-portfolio.git";
   return (
     <>
       <div id="projects">
@@ -40,7 +40,7 @@ const Projects = () => {
                       <div className="bufw1"><FaLaptopCode className='servicon1' /></div>
                       <div className="bufw1"><p>Web Development</p></div>
                     </div>
-                    <a className='a1' href="#!">View Code</a>
+                    <a className='a1' href={portfolio_project_sourcecode_link}>View Code</a>
                     <a href="#!" className="btn btn1 btn-primary">Live Preview</a>
                   </div>
                 </div>
